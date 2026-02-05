ec27e0e0d26634493dd8c73285092f9e83ffe329
Garima <moghagarima26email@gmail.com>
2026-02-05 18:41:56 +0000

    Code challenge submission for question: 3427 Sum of Variable Length Subarrays

---
+++ 3427 Sum of Variable Length Subarrays/3427sum-of-variable-length-subarrays.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums) {
+        int n = nums.size();
+        int totalSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            int start = max(0, i - nums[i]);
+            int sum = 0;
+
+            for (int j = start; j <= i; j++) {
+                sum += nums[j];
+            }
+
+            totalSum += sum;
+        }
+
+        return totalSum;
+    }
+};
