fc86dace7fbeac813d69c596579176f3fcab3569
Garima <moghagarima26email@gmail.com>
2026-02-16 19:35:26 +0000

    Code challenge submission for question: 2154 Keep Multiplying Found Values by Two

---
+++ 2154 Keep Multiplying Found Values by Two/2154keep-multiplying-found-values-by-two.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        int n= nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i]==original){
+     return findFinalValue(nums, original * 2);
+
+            }
+        }
+        return original;
+        
+    }
+};
