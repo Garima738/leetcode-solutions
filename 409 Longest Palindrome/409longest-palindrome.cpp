d86448625ce0646018aeb340d4a322373e240da0
Garima <moghagarima26email@gmail.com>
2026-05-10 20:27:48 +0000

    Code challenge submission for question: 409 Longest Palindrome

---
+++ 409 Longest Palindrome/409longest-palindrome.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        unordered_map<char, int> freq;
+
+     
+        for(char ch : s) {
+            freq[ch]++;
+        }
+
+        int length = 0;
+        bool oddFound = false;
+
+        for(auto it : freq) {
+            int count = it.second;
+
+            if(count % 2 == 0) {
+                length += count;
+            } else {
+                length += count - 1;
+                oddFound = true;
+            }
+        }
+
+        
+        if(oddFound) {
+            length += 1;
+        }
+
+        return length;
+    }
+};
