2b3037eb3f20e91da6b0cf240750080329e42765
Garima <moghagarima26email@gmail.com>
2026-05-27 13:02:14 +0000

    Code challenge submission for question: 240 Search a 2D Matrix II

---
+++ 240 Search a 2D Matrix II/240search-a-2d-matrix-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row=0;
+        int col=matrix[0].size()-1;
+        while(row<matrix.size()&&col>=0){
+            int element = matrix[row][col];
+            if(element==target){
+                return true;
+            }
+            if(element>target){
+                col--;
+            }
+            else{
+                row++;
+            }
+        }
+        return false;
+
+    }
+};
