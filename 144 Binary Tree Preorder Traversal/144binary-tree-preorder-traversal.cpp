6f71a6d54a637ddc392ed89825c0f1ea4e39b7a9
Garima <moghagarima26email@gmail.com>
2026-06-09 09:42:48 +0000

    Code challenge submission for question: 144 Binary Tree Preorder Traversal

---
+++ 144 Binary Tree Preorder Traversal/144binary-tree-preorder-traversal.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> arr;
+        preorder(root, arr);
+        return arr;
+    }
+
+    void preorder(TreeNode* root, vector<int>& arr) {
+        if (root == NULL) {
+            return;
+        }
+
+        arr.push_back(root->val);  // Root
+        preorder(root->left, arr); // Left
+        preorder(root->right, arr); // Right
+    }
+};
